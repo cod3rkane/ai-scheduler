15b7be2d1191c4c265c0d8ccc9a3076a1c6cce37
Kronos <3195760+cod3rkane@users.noreply.github.com>
2026-04-14 11:01:58 -0400

    API Example

--- modern.config.ts
+++ modern.config.ts
@@ -1,9 +1,10 @@
 import {appTools, defineConfig} from '@modern-js/app-tools';
+import { bffPlugin } from '@modern-js/plugin-bff';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   server: {
     ssr: true,
   },
-  plugins: [appTools()],
+  plugins: [appTools(), bffPlugin()],
 });
