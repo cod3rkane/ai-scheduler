bf17faeba331370dc0b3ab05a1f6c7a5291055ff
Kronos <3195760+cod3rkane@users.noreply.github.com>
2026-04-15 09:26:36 -0400

    Fixes PostCSS Loading

--- modern.config.ts
+++ modern.config.ts
@@ -1,5 +1,4 @@
 import {appTools, defineConfig} from '@modern-js/app-tools';
-import tailwindcssPlugin from '@modern-js/plugin-tailwindcss';
 import {bffPlugin} from '@modern-js/plugin-bff';
 
 // https://modernjs.dev/en/configure/app/usage
@@ -8,4 +7,13 @@ export default defineConfig({
     ssr: true,
   },
   plugins: [appTools(), bffPlugin()],
+  tools: {
+    postcss: {
+      postcssOptions: {
+        plugins: [
+          require('@tailwindcss/postcss')
+        ]
+      }
+    }
+  }
 });
